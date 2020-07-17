43a690fb21771eac9f6f6bfe2462f7b5fc45e8d3
ChristineWasike <christinewasike22@gmail.com>
2020-07-18 00:28:01 +0300

    Simple plotly graph rep

---
+++ dash_app.py
@@ -0,0 +1,22 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+app.layout = html.Div(children=[
+    html.H1('Dash App'),
+    dcc.Graph(id='examples',
+              figure={
+                  # Plotly Graph. Simple rep
+                  'data': [
+                      {'x': [1, 2, 3, 4, 5], 'y': [5, 8, 1, 6, 5], 'type': 'line', 'name': 'boats'},
+                      {'x': [1, 2, 3, 4, 5], 'y': [5, 4, 3, 2, 1], 'type': 'bar', 'name': 'cars'},
+                  ],
+                  'layout': {
+                      'title': 'Basic Sample'
+                  }
+              })
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
