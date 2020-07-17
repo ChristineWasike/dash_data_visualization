d73e2daf16d63000249836afd24c675154028ff0
ChristineWasike <christinewasike22@gmail.com>
2020-07-18 00:44:10 +0300

    Done the ineractive user interface section

--- dash_app.py
+++ dash_app.py
@@ -1,22 +1,22 @@
 import dash
+# Introduced a new import
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash()
 app.layout = html.Div(children=[
-    html.H1('Dash App'),
-    dcc.Graph(id='examples',
-              figure={
-                  # Plotly Graph. Simple rep
-                  'data': [
-                      {'x': [1, 2, 3, 4, 5], 'y': [5, 8, 1, 6, 5], 'type': 'line', 'name': 'boats'},
-                      {'x': [1, 2, 3, 4, 5], 'y': [5, 4, 3, 2, 1], 'type': 'bar', 'name': 'cars'},
-                  ],
-                  'layout': {
-                      'title': 'Basic Sample'
-                  }
-              })
+    dcc.Input(id='input', value='Type something', type='text'),
+    html.Div(id='output')
 ])
+
+
+@app.callback(
+    Output(component_id='output', component_property='children'),
+    [Input(component_id='input', component_property='value')])
+def update_value(input_data):
+    return "Input: {}".format(input_data)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
